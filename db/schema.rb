d3636c98f5f104168bf2b4058e503c86c0e93dac
Joan Currie <jecurrie8@gmail.com>
2019-11-25 20:20:54 -0500

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120215905) do
+ActiveRecord::Schema.define(version: 20191125225530) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
